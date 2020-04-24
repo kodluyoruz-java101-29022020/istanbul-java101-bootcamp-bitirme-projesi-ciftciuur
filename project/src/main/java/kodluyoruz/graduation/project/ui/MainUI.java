7456d266ec27b20196f7432ac75ad875d86f6baf
ugur ciftci <ugurprog@gmail.com>
2020-04-24 09:42:09 +0300

    change project server port 7070
    and main ui notification test

--- project/src/main/java/kodluyoruz/graduation/project/ui/MainUI.java
+++ project/src/main/java/kodluyoruz/graduation/project/ui/MainUI.java
@@ -6,6 +6,7 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.spring.server.SpringVaadinServlet;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 
 @SpringUI
@@ -13,7 +14,7 @@ public class MainUI extends UI {
 
 	@Override
 	protected void init(VaadinRequest request) {
-		// TODO Auto-generated method stub
+		Notification.show("Test");
 
 	}
 
